531cbd80157849af9b3ce7731df32f2c061bcdee
Minseok Kim^C <ksj5147@gmail.com>
2024-11-01 14:00:16 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
   const [dropzoneActive, setDropzoneActive] = useState(false)
   const [files, setFiles] = useState([])
+  const [currentFileIndex, setCurrentFileIndex] = useState(null)
+  const [lastUploadedFileIndex, setLastUploadedFileIndex] = useState(null)
+  const [currentChunkIndex, setCurrentChunkIndex] = useState(null)
 
   function handleDrop (e) {
     e.preventDefault()
-    setFiles([...e.dataTransfer.files, ...files])
+    setFiles([...files, ...e.dataTransfer.files])
   }
+
+  function readAndUploadCurrentChunk () {}
+
+  useEffect(() => {
+    if (files.length > 0) {
+      if (currentFileIndex === null) {
+        setCurrentFileIndex(
+          lastUploadedFileIndex === null ? 0 : lastUploadedFileIndex + 1
+        )
+      }
+    }
+  }, [files.length])
+
+  useEffect(() => {
+    if (currentFileIndex !== null) {
+      setCurrentChunkIndex(0)
+    }
+  }, [currentFileIndex])
+
+  useEffect(() => {
+    if (currentChunkIndex !== null) {
+      readAndUploadCurrentChunk()
+    }
+  }, [currentChunkIndex])
 
   return (
     <div>
