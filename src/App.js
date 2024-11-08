6dd329219c73956ddaf4a153d76ee0e740a8fe44
Minseok Kim^C <ksj5147@gmail.com>
2024-11-07 22:06:12 -0800

    login

--- src/App.js
+++ src/App.js
@@ -19,11 +19,10 @@ function App () {
   function readAndUploadCurrentChunk () {
     const reader = new FileReader()
     const file = files[currentFileIndex]
-    if (!files) {
+    if (!file) {
       return
     }
     const from = currentChunkIndex * chunkSize
-
     const to = from + chunkSize
     const blob = file.slice(from, to)
     reader.onload = e => uploadChunk(e)
@@ -39,7 +38,7 @@ function App () {
     params.set('currentChunkIndex', currentChunkIndex)
     params.set('totalChunks', Math.ceil(file.size / chunkSize))
     const headers = { 'Content-Type': 'application/octet-stream' }
-    // eslint-disable-next-line no-undef
+    const url = 'http://localhost:4001/upload?' + params.toString()
     axios.post(url, data, { headers }).then(response => {
       const file = files[currentFileIndex]
       const filesize = files[currentFileIndex].size
@@ -54,6 +53,15 @@ function App () {
       }
     })
   }
+
+  useEffect(() => {
+    if (lastUploadedFileIndex === null) {
+      return
+    }
+    const isLastFile = lastUploadedFileIndex === files.length - 1
+    const nextFileIndex = isLastFile ? null : currentFileIndex + 1
+    setCurrentFileIndex(nextFileIndex)
+  }, [lastUploadedFileIndex])
 
   useEffect(() => {
     if (files.length > 0) {
@@ -92,6 +100,37 @@ function App () {
         className={'dropzone' + (dropzoneActive ? ' active' : '')}
       >
         Drop your files here
+      </div>
+      <div className='files'>
+        {files.map((file, fileIndex) => {
+          let progress = 0
+          if (file.finalFilename) {
+            progress = 100
+          } else {
+            const uploading = fileIndex === currentFileIndex
+            const chunks = Math.ceil(file.size / chunkSize)
+            if (uploading) {
+              progress = Math.round((currentChunkIndex / chunks) * 100)
+            } else {
+              progress = 0
+            }
+          }
+          return (
+            <a
+              className='file'
+              target='_blank'
+              href={'http://localhost:4001/uploads/' + file.finalFilename}
+            >
+              <div className='name'>{file.name}</div>
+              <div
+                className={'progress ' + (progress === 100 ? 'done' : '')}
+                style={{ width: progress + '%' }}
+              >
+                {progress}%
+              </div>
+            </a>
+          )
+        })}
       </div>
     </div>
   )
